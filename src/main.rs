b6a813707b5d065d8cde6468dda326fb2bea9237
Redstoyn33 <36273864+redstoyn33@users.noreply.github.com>
2024-07-23 21:27:28 +0300

    fix I don't know what

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use crate::app::DamageCalcApp;
-
-mod app;
-
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
@@ -24,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(DamageCalcApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(DamageCalc::DamageCalcApp::new(cc)))),
     )
 }
 
