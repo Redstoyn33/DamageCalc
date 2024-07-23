aad6001525aaed6e28ee92fb2dcfa9429b632559
Redstoyn33 <36273864+redstoyn33@users.noreply.github.com>
2024-07-23 20:34:44 +0300

    name change & template code delete

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'DamageCalc-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './DamageCalc.js',
+  './DamageCalc_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
