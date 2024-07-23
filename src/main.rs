3de3ec5798143c6d5916289b5720a485e5425723
Redstoyn33 <36273864+redstoyn33@users.noreply.github.com>
2024-07-23 20:34:44 +0300

    name change & template code delete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use crate::app::DamageCalcApp;
+
+mod app;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
@@ -20,7 +24,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(DamageCalcApp::new(cc)))),
     )
 }
 
@@ -37,7 +41,7 @@ fn main() {
             .start(
                 "the_canvas_id",
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(DamageCalc::TemplateApp::new(cc)))),
             )
             .await;
 
